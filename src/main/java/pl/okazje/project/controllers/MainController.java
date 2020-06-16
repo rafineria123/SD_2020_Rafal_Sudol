717599a1ffb62de0220ba32e1d070366ef91326f
rafineria123 <37177277+rafineria123@users.noreply.github.com>
2020-06-16 12:12:35 +0200

    5th commit - front

--- src/main/java/pl/okazje/project/controllers/MainController.java
+++ src/main/java/pl/okazje/project/controllers/MainController.java
@@ -1,9 +1,12 @@
 package pl.okazje.project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,6 +16,7 @@ import pl.okazje.project.repositories.*;
 import javax.jws.soap.SOAPBinding;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -31,96 +35,13 @@ public class MainController {
     @Autowired
     CommentRepository commentRepository;
 
+    private static User uzytkownik = null;
+
     @GetMapping("/")
     public ModelAndView homePage() {
 
-        Shop shop1 = new Shop("Biedronka","images/shop_1.png");
-        Shop shop2 = new Shop("Avon","images/shop_2.png");
-        Shop shop3 = new Shop("X-kom" ,"images/shop_3.png");
-        Shop shop4 = new Shop("Tesco","images/shop_4.png");
-        Shop shop5 = new Shop("Empik" ,"images/shop_3.png");
-        Shop shop6 = new Shop("Media Markt","images/shop_4.png");
-
-
-        shopRepository.save(shop1);
-        shopRepository.save(shop2);
-        shopRepository.save(shop3);
-        shopRepository.save(shop4);
-        shopRepository.save(shop5);
-        shopRepository.save(shop6);
-
-
-        Tag tag1 = new Tag("Elektronika");
-        Tag tag2 = new Tag("Motoryzacja");
-        Tag tag3 = new Tag("Gaming");
-        Tag tag4 = new Tag("Moda");
-        Tag tag5 = new Tag("Artykuły Spożywcze");
-        Tag tag6 = new Tag("Sport i turystyka");
-
-        tagRepository.save(tag1);
-        tagRepository.save(tag2);
-        tagRepository.save(tag3);
-        tagRepository.save(tag4);
-        tagRepository.save(tag5);
-        tagRepository.save(tag6);
-
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        User user1 = new User("rafineria",encoder.encode("password"),"rafineria@gmail.com");
-        User user2 = new User("rafineria123",encoder.encode("password123"),"rafineria123@gmail.com");
-
-        userRepository.save(user1);
-        userRepository.save(user2);
-
-        //String title, String content, String image_url, Double old_price, Double current_price, Double shipment_price, String discount_link, User user, Tag tag, Shop shop
-
-        Discount discount1 = new Discount("Baterie alkaliczne AA",
-                "W Kaufland dostępne w dobrej cenie baterie alkaliczne AA oraz AAA 10szt. (wychodzi 1zł za sztukę). Dostępna duża ilość. Znalezione w Pszczynie, ale z informacji innych użytkowników okazja ogólnopolska.",
-                "images/discount_1.jpg",255.0,220.0,15.0,"https://www.biedronka.pl/pl/dom-nf-10-06",user1,tag1,shop6);
-        Discount discount2 = new Discount("Brzoskwinia @ Biedronka",
-                "Oferta obowiązuje od 12.06 do 13.06.2020 r.",
-                "images/discount_2.jpg",10.0,5.0,0.0,"https://www2.pl.avon.com/pl-home",user1,tag5,shop1);
-        Discount discount3 = new Discount("Xbox Game Pass - Czerwiec",
-                "W ramach usługi Xbox Game Pass w czerwcu zagramy w : Xbox Game Pass - nowe gry Battlefleet Gothic Armada 2 (PC) – 11 czerwca Battletech (PC) – 11 czerwca Dungeon of the End.",
-                "images/discount_3.jpg",100.0,1.0,0.0,
-                "https://www.x-kom.pl/p/568960-smartfon-telefon-realme-x3-superzoom-12256gb-glacier-blue-120hz.html",user1,tag3,shop5);
-        Discount discount4 = new Discount("Kawa Dallmayr Classic Krafti.",
-                "W Biedronce od 12.06 kawa mielona Dallmayr Classic Krafti.",
-                "images/discount_4.jpg",20.0,15.0,0.0,"https://www2.pl.avon.com/pl-home",user2,tag3,shop1);
-        Discount discount5 = new Discount("IPRee namiot 3 w 1 dla 5-8 osób",
-                "Dostawa gratis Przecena z 129.99$ na 84.99$. Wersja S kosztuje po przecenie 55.24$.",
-                "images/discount_5.jpg",500.0,350.0,10.0,"https://www2.pl.avon.com/pl-home",user2,tag6,shop4);
-        Discount discount6 = new Discount("Płyn do chłodnic samochodowych",
-                "W Tesco w Jarosławiu przecena płynu zimowego Borygo. Zostało jeszcze minimum 10 sztuk. Oferta lokalna. Małe miasto, ale może komuś się przyda ta informacja.",
-                "images/discount_6.jpg",35.0,17.0,0.0,"https://www2.pl.avon.com/pl-home",user2,tag2,shop2);
-
-
-        discountRepository.save(discount1);
-        discountRepository.save(discount2);
-        discountRepository.save(discount3);
-        discountRepository.save(discount4);
-        discountRepository.save(discount5);
-        discountRepository.save(discount6);
-
-        Comment comment1 = new Comment("Słownik podpowiedział nie to słowo co powinien. Moja wina że nie sprawdziłem. Ale nie zmienia to faktu że komentarze" +
-                " w tym stylu co napisałeś są bez sensu. Co to ma wnosić do obecnej sytuacji." +
-                " Kiedyś może mogło być drożej, mogło być taniej. Dzisiaj taka cena jest najtańsza i na tym polega ten serwis.");
-        comment1.setUser(discount3.getUser());
-        comment1.setDiscount(discount3);
-        commentRepository.save(comment1);
-
-        Rating rating1 = new Rating();
-        rating1.setUser(discount3.getUser());
-        rating1.setDiscount(discount3);
-        Rating rating2 = new Rating();
-        rating2.setUser(discount3.getUser());
-        rating2.setDiscount(discount3);
-        Rating rating3 = new Rating();
-        rating3.setUser(discount3.getUser());
-        rating3.setComment(comment1);
-        ratingRepository.save(rating1);
-        ratingRepository.save(rating2);
-        ratingRepository.save(rating3);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        uzytkownik = userRepository.findUserByLogin(authentication.getName());
 
 
         ModelAndView modelAndView = new ModelAndView("home");
@@ -132,6 +53,7 @@ public class MainController {
 
     @GetMapping("/categories/{id}")
     public ModelAndView category(@PathVariable("id") String id) {
+
         List<Discount> list = new ArrayList<>();
         for (Discount discount: discountRepository.findAll()) {
             if(discount.getTag().getName().toLowerCase().equals(id.toLowerCase())){
@@ -182,22 +104,96 @@ public class MainController {
     @GetMapping("/add/discount")
     public ModelAndView add_discount() {
         ModelAndView modelAndView = new ModelAndView("add_discount");
+        modelAndView.addObject("list_of_tags", tagRepository.findAll());
+        modelAndView.addObject("list_of_shops", shopRepository.findAll());
         return modelAndView;
     }
     @GetMapping("/login")
     public ModelAndView login() {
         ModelAndView modelAndView = new ModelAndView("login");
+        modelAndView.addObject("list_of_tags", tagRepository.findAll());
+        modelAndView.addObject("list_of_shops", shopRepository.findAll());
         return modelAndView;
     }
 
     @GetMapping("/register")
     public ModelAndView register() {
         ModelAndView modelAndView = new ModelAndView("register");
+        modelAndView.addObject("list_of_tags", tagRepository.findAll());
+        modelAndView.addObject("list_of_shops", shopRepository.findAll());
         return modelAndView;
     }
 
+    @PostMapping("/addrate")
+    public String addrate(@ModelAttribute("discountidadd") String discountid){
 
+        System.out.println(discountid);
 
+        Discount discount = discountRepository.findById(Long.parseLong(discountid)).get();
+        for(Rating r:discount.getRatings()){
 
+            if(r.getUser().getUser_id().equals(uzytkownik.getUser_id())){
+                System.out.println("2");
+                return "redirect:/";
+
+            }
+
+        }
+
+        System.out.println("3");
+
+        Rating newrating = new Rating();
+        newrating.setUser(uzytkownik);
+        newrating.setDiscount(discount);
+        ratingRepository.save(newrating);
+
+        return "redirect:/";
+
+    }
+
+    @PostMapping("/removerate")
+    public String removerate(@ModelAttribute("discountidremove") String discountid){
+
+        Discount discount = discountRepository.findById(Long.parseLong(discountid)).get();
+        for(Rating r:discount.getRatings()){
+
+            if(r.getUser().getUser_id().equals(uzytkownik.getUser_id())){
+                ratingRepository.delete(r);
+                return "redirect:/";
+
+            }
+
+        }
+
+        return "redirect:/";
+
+    }
+
+    @PostMapping("/addcomment")
+    public String addcomment(@ModelAttribute("discountidaddcomment") String discountaddcomment, @ModelAttribute("comment") String comment){
+
+        Comment comment1 = new Comment();
+        comment1.setDiscount(discountRepository.findById(Long.parseLong(discountaddcomment)).get());
+        comment1.setUser(uzytkownik);
+        comment1.setContent(comment);
+        comment1.setCr_date(new Date());
+        commentRepository.save(comment1);
+
+        return "redirect:/discount/"+Long.parseLong(discountaddcomment);
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute("login") String login, @ModelAttribute("password") String password, @ModelAttribute("email") String email){
+
+        User user1 = new User();
+        user1.setCr_date(new Date());
+        user1.setLogin(login);
+        BCryptPasswordEncoder enkoder = new BCryptPasswordEncoder();
+        user1.setPassword(enkoder.encode(password));
+        user1.setEmail(email);
+        userRepository.save(user1);
+
+        return "redirect:/login";
+    }
 
 }
--- src/main/java/pl/okazje/project/repositories/UserRepository.java
+++ src/main/java/pl/okazje/project/repositories/UserRepository.java
@@ -1,8 +1,15 @@
 package pl.okazje.project.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.okazje.project.entities.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {}
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query(value = "SELECT * FROM user where login =?1 LIMIT 1" ,
+            nativeQuery = true)
+    public User findUserByLogin(String login);
+
+}
