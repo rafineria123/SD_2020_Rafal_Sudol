525c5ae6d1ded6276197c2d0bd179d60a0b81af0
rafineria123 <37177277+rafineria123@users.noreply.github.com>
2021-04-07 23:41:18 +0200

    non-imporant changes

--- src/main/java/pl/okazje/project/controllers/PostController.java
+++ src/main/java/pl/okazje/project/controllers/PostController.java
@@ -1,6 +1,5 @@
 package pl.okazje.project.controllers;
 
-import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
